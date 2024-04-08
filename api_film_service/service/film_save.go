a24968f9f6a73db1e8fd10865fadb30ef84b9de8
tcweeei <open891013@gmail.com>
2024-04-08 16:40:10 +0800

    Fix the bug when context cancel

--- api_film_service/service/film_save.go
+++ api_film_service/service/film_save.go
@@ -2,10 +2,7 @@ package service
 
 import (
 	pb "api_film_service/proto/film_service"
-	"api_film_service/repository/model"
 	"context"
-	"log"
-	"time"
 )
 
 func (s filmService) SaveFilm(ctx context.Context, request *pb.FilmSaveRequest) error {
@@ -16,34 +13,7 @@ func (s filmService) SaveFilm(ctx context.Context, request *pb.FilmSaveRequest) 
 		request.Directors = []string{}
 	}
 
-	err := s.mongodbRepository.SaveFilm(ctx, request)
-	if err != nil {
-		return err
-	}
-	go func() {
-		film := model.Film{
-			Id:           request.Id,
-			Title:        request.Title,
-			Type:         request.Type.String(),
-			Category:     request.Category.String(),
-			Location:     request.Location.String(),
-			Language:     request.Language,
-			ReleaseYear:  request.ReleaseYear,
-			Resource:     request.Resource,
-			State:        request.State,
-			Popularity:   0,
-			Introduction: request.Introduction,
-			Actors:       request.Actors,
-			Directors:    request.Directors,
-			UpdateAt:     time.Now().UTC(),
-			CreatedAt:    time.Now().UTC(),
-		}
-		err := s.elasticsearchRepository.FilmSave(ctx, film)
-		if err != nil {
-			log.Println("error to add film id (" + request.Id + ") to search engine with " + err.Error())
-		}
-	}()
-	return nil
+	return s.mongodbRepository.SaveFilm(ctx, request)
 }
 func (s filmService) SaveFilmEpisode(ctx context.Context, request *pb.FilmSaveEpisodeRequest) error {
 	return s.mongodbRepository.SaveFilmEpisode(ctx, request)
