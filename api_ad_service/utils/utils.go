53bf2cd9da32d790897e80dae34b37cbea5a4264
tcweeei <open891013@gmail.com>
2024-04-08 14:20:45 +0800

    Add the singleflight setting

---
+++ api_ad_service/utils/utils.go
@@ -0,0 +1,13 @@
+package utils
+
+const (
+	defaultTimeLimitSecond   = 1
+	defaultForgetMilliSecond = 100
+)
+
+func GetDefaultTimeLimitSecond() int {
+	return defaultTimeLimitSecond
+}
+func GetDefaultForgetMilliSecond() int {
+	return defaultForgetMilliSecond
+}
