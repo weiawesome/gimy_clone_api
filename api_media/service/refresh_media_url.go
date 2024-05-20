d9ec8569f32064a8df61bff747a320871229bbd3
tcweeei <open891013@gmail.com>
2024-05-20 20:24:54 +0800

    Update the prefix url of the key

--- api_media/service/refresh_media_url.go
+++ api_media/service/refresh_media_url.go
@@ -6,6 +6,9 @@ import (
 )
 
 func (s mediaService) RefreshMediaUrl(playlist *m3u8.MediaPlaylist) {
+	if playlist.Key != nil {
+		playlist.Key.URI = utils.EnvCDNAddress() + "/api/" + utils.GetVersion() + "/resource/media/" + playlist.Key.URI
+	}
 	for _, segment := range playlist.Segments {
 		if segment != nil {
 			segment.URI = utils.EnvCDNAddress() + "/api/" + utils.GetVersion() + "/resource/media/" + segment.URI
